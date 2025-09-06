78e4a60290b61401ce9cbbedf2b344f026810c75
vishvjeet07 <vishvjeetwankhade07@gmail.com>
2025-09-07 02:08:13 +0530

    url added

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI+"whispervault");
+        const conn = await mongoose.connect(process.env.MONGO_URI+"/Discuze");
         console.log(`MongoDB connection established successfully`);
     } catch (error) {
         console.log('Database Connection error');
