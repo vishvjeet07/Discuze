9fa3be5c2376845b910836cebc7d8f8abf76db33
vishvjeet07 <vishvjeetwankhade07@gmail.com>
2025-09-07 13:41:41 +0530

    HashRouter Added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import  { BrowserRouter } from 'react-router-dom'
+import  { BrowserRouter, HashRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <HashRouter>
       <AppContextProvider>
           <App />
       </AppContextProvider>
-    </BrowserRouter>
+    </HashRouter>
 )
