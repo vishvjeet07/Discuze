c615fbecb980c139e945b171fc2eb6b88d7e7cec
vishvjeet07 <vishvjeetwankhade07@gmail.com>
2025-09-07 13:52:58 +0530

    optimised

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,18 +5,12 @@ import jwt from 'jsonwebtoken'
 export const register = async (req,res) =>{
     const { username, email, password } = req.body;
     if(username == "" || email == "" || password == ""){
-        return res.status(400).json({ 
-        message: "All fields are required",
-        success: false 
-        });
+        res.json({ success: false, message: "All fields are required"});
     }
     try {
         let user = await User.findOne({email});
         if(user){
-                return res.status(400).json({ 
-                message: "User already exists",
-                success: false 
-            });
+        res.json({ success: false, message: "User already exists"});
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -34,11 +28,11 @@ export const register = async (req,res) =>{
 
         res.cookie('token',token);
 
-        res.json({ message: "account created ",token, success: true });
+        res.json({ success: true, message: "Account created ",token});
         
     } catch (error) {
         console.log(error);
-        res.json({ message: "account not created ", success: false });
+        res.json({ success: false, message: "Account not created " });
     }
     
 }
@@ -47,28 +41,19 @@ export const register = async (req,res) =>{
 export const login = async (req,res) =>{
     const { email, password } = req.body;
     if(email == "" || password == ""){
-        return res.status(204).json({
-            message: "please enter email or password",
-            success: false
-        });
+        res.json({success: false, message: "Please enter email or password"});
     }
 
     try {
         const user = await User.findOne({email});
         if(!user){
-            return res.status(404).json({ 
-            message: "User not found",
-            success: false 
-            });
+            res.json({ success: false, message: "User not found"});
         }
 
         const isPasswordValid = await bcrypt.compare(password,user.password);
 
         if(!isPasswordValid){
-                return res.status(401).json({ 
-                message: "Invalid credentials",
-                success: false 
-            });
+                res.json({ success: false, message: "Invalid credentials"});
         }
 
         const token = jwt.sign(
@@ -76,25 +61,18 @@ export const login = async (req,res) =>{
             process.env.JWT,
         );
         res.cookie('token',token);
-        res.json({ message: "account created ",token, success: true });
+        res.json({success: true, message: "Login successful ",token});
     }catch (error) {
-        console.error("Login error:", error);
-        res.status(500).json({ 
-            message: "Server error",
-            success: false 
-        });
+        res.json({ success: false, message: "Server error"});
     }
 }
 
 export const logout = async (req,res) => {
     try {
         res.cookie("token","");
-        res.json({success:true,message:"Logout Successfully"})
+        res.json({success:true, message:"Logout Successful"})
     } catch (error) {
         console.error("Login error:", error);
-        res.json({ 
-            message: "Server error",
-            success: false 
-        });
+        res.json({ success: false, message: "Server error"});
     }
 }
